ba104074a8c20b330c65db827c04ba9d1ca931db
bookyo <quyue@quyuedeMac-mini.local>
2022-09-30 14:00:06 +0800

    add multer get post data

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+const callbackUpload = multer();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/payback', async (req, res) => {
+router.post('/payback', callbackUpload.array(), async (req, res) => {
   console.log(req.body);
   return res.send('fail');
 })
